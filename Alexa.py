32ae7b122568c9452d9139e0fc5506a68ef41f9b
Aki Wada <115308304+akiwadajp@users.noreply.github.com>
2023-04-13 00:13:38 -0500

    Update Alexa.py

--- Alexa.py
+++ Alexa.py
@@ -1,43 +1,42 @@
-import speech recognition as sr
+import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
 
-listeners = sr.Recognizer()
+listener = sr.Recognizer()
 engine = pyttsx3.init()
-voices = engine.getPorperty("voices")
+voices = engine.getProperty("voices")
 engine.setProperty('voice', voices[1].id)
 
 def talk(text):
-  engine.say(text)
-  engine.runAndWait()
-  
+    engine.say(text)
+    engine.runAndWait()
+
 def take_command():
-  try:
-    talk(info)def take command):
-  
- try:
-  with sr.Microphone() as source:
-    print('listening ...')
-    voice = listern.listen(source)
-    command = listener.recognize_google(voice)
-    command = command.lower()
-    if 'alexa' in command:
-      command = command.replace('alexa', '')
-      print(command)
- 
-except:
-  pass
-return command
+    try:
+        with sr.Microphone() as source:
+            print('listening ...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
 
 def run_alexa():
-  command = take_command()
-  print(command)
-  if 'play' in command:
-    song = command.replace('play', '')
-    talk('Playing ' + song)
-    pywhatkit.playonyt(song)
-  elif 'time' in command:
-    time = datetime.datetime.now()
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('Playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now()
+        talk('Current time is ' + time.strftime('%I:%M %p'))
+
+run_alexa()
